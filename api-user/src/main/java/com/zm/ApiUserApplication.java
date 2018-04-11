2ef313522e171bbb30d36538caa3a610db6cf09e
e <tianshenglongchang@126.com>
2018-04-11 15:59:15 +0800

    完成api-user

--- api-user/src/main/java/com/zm/ApiUserApplication.java
+++ api-user/src/main/java/com/zm/ApiUserApplication.java
@@ -2,15 +2,13 @@ package com.zm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@SpringBootApplication
+@EnableEurekaClient
 @RestController
-@EnableFeignClients
-@SpringCloudApplication
 public class ApiUserApplication {
 
 	public static void main(String[] args) {
