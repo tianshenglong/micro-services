7819a216d4956d26b0b9ed0db92ab7d17d61818c
e <tianshenglongchang@126.com>
2018-04-11 18:17:03 +0800

    增加

--- api-shop/src/main/java/com/zm/ApiShopApplication.java
+++ api-shop/src/main/java/com/zm/ApiShopApplication.java
@@ -3,23 +3,21 @@ package com.zm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringCloudApplication
-//需要feign调用时 必须加此注解
+@RestController
 @EnableFeignClients
-public class ApiUserApplication {
+public class ApiShopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiUserApplication.class, args);
+		SpringApplication.run(ApiShopApplication.class, args);
 	}
 
 	@RequestMapping("/")
 	public String index() throws Exception {
-		return "micro-service-api-user";
+		return "micro-service-api-shop";
 	}
 }
