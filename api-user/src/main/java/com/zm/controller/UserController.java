1e100714d60f01d870bee45f4b567ef076e7148b
e <tianshenglongchang@126.com>
2018-05-10 18:52:57 +0800

    增加swagger构建RESTful APIDOCs

--- api-user/src/main/java/com/zm/controller/UserController.java
+++ api-user/src/main/java/com/zm/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.zm.controller;
 
 import com.zm.feign.client.ShopFeignClient;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +20,8 @@ public class UserController {
     @Autowired
     ShopFeignClient shopFeignClient;
 
+    @ApiOperation(value = "简单输出你好",notes = "简单输出你好，用于测试")
+    //@ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long")//指明参数
     @GetMapping
     public String sayHello(){
         return "hello world";
